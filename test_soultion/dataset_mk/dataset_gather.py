5aec5f3da61a24761595b17f1bdbd346bc498be8
KimJungHyun <72688495+kick9850@users.noreply.github.com>
2023-01-19 15:42:23 +0900

    Add files via upload

---
+++ test_soultion/dataset_mk/dataset_gather.py
@@ -0,0 +1,109 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+class PoseDetector:
+    def __init__(self, mode = False, upBody = False, smooth=True, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
+        return img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS
+
+    def getPosition(self, img, draw=True):
+        lmList= []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return lmList
+
+video_dir="train_video/*.mp4"
+index=len(video_dir)
+gesture = {
+    0: 'foot dance', 1: 'breathing', 2: 'arm dance'
+}  # 3가지의 제스처, 제스처 데이터는 손가락 관절의 각도와 각각의 라벨을 뜻한다.
+
+# MediaPipe hands model
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+pose = mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+# Gesture recognition data
+file = np.genfromtxt('seq/gesture_train.csv', delimiter=',')
+print(file.shape)
+
+cap = cv2.VideoCapture(0)
+
+
+def click(event, x, y, flags, param):
+    global data, file
+    if event == cv2.EVENT_LBUTTONDOWN:
+        file = np.vstack((file, data))
+        print(file.shape)
+
+
+cv2.namedWindow('Dataset')
+cv2.setMouseCallback('Dataset', click)
+
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        continue
+
+    result = pose.process(img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    if result.draw_landmarks is not None:
+        for res in result.draw_landmarks:
+            joint = np.zeros((21, 3))
+            for j, lm in enumerate(res.landmark):
+                joint[j] = [lm.x, lm.y, lm.z]
+
+            # Compute angles between joints
+            v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19], :]  # Parent joint
+            v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], :]  # Child joint
+            v = v2 - v1  # [20,3]
+            # Normalize v
+            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+            # Get angle using arcos of dot product
+            angle = np.arccos(np.einsum('nt,nt->n',
+                                        v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
+                                        v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))  # [15,]
+
+            angle = np.degrees(angle)  # Convert radian to degree
+
+            data = np.array([angle], dtype=np.float32)
+
+            data = np.append(data, 11)  #
+
+            mp_drawing.draw_landmarks(img, res, mp_pose.POSE_CONNECTIONS)
+
+    cv2.imshow('Dataset', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+np.savetxt('data/gesture_train_fy.csv', file, delimiter=',')
